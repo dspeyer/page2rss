ac90c37ef3da121fe5909243769f4534454970e8
Daniel Speyer <dspeyer@gmail.com>
2017-07-26 14:52:49 -0400

    test feed maybe working

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import webapp2
 from feedgen.feed import FeedGenerator
 from datetime import datetime
+from pytz import utc
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -11,13 +12,13 @@ class TestFeed(webapp2.RequestHandler):
     def get(self):
         fg=FeedGenerator()
         fg.title('Test RSS Feed')
-        fg.id('http://page2rss-174917.appspot.com/')
+        fg.link(href='http://page2rss-174917.appspot.com/')
         fg.description('Had this been an actual RSS feed...')
         for i in range(4):
             fe = fg.add_entry()
             fe.title('Test entry %d' % i)
-            fe.link('http://page2rss-174917.appspot.com/%d' % i)
-            fe.pubdate(datetime(2017,i,4))
+            fe.link(href='http://page2rss-174917.appspot.com/%d' % i)
+            fe.pubdate(datetime(2017,i+1,4,tzinfo=utc))
         self.response.headers['Content-Type'] = 'application/rss+xml'
         self.response.write(fg.rss_str(pretty=True))
             
